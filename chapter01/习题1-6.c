accdef874786bd24d8440ff5f2e8b122b5852513
日沉云起 <csjiangfeng1212@163.com>
2019-04-21 21:04:42 +0800

    Update 习题1-6.c

--- chapter01/习题1-6.c
+++ chapter01/习题1-6.c
@@ -4,7 +4,7 @@ int main(){
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
     if(a+b>c&&a+c>b&&b+c>a)
-        if(a*a+b*b==c*c||a*a+b*b==c*c||b*b+c*c==a*a)
+        if(a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a)
             printf("yes\n");
         else
             printf("no\n");
